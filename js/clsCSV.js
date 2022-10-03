d8df95f44474962cd75ea796383e8ec6c0da4d02
myhamidi <myhamidi@hotmail.de>
2022-10-03 13:08:28 +0200

    Tag SVG

--- js/clsCSV.js
+++ js/clsCSV.js
@@ -287,7 +287,7 @@ class clsCSV {
     Edit(divID) {
         this._HighlightCell(divID);
         this._CreateInputField(divID)
-        this._CreateSaveSVG(divID)
+        this._svgAppend_Save(divID)
         this._CreateRevertX(divID)
         this._CreateName(divID)
         
@@ -313,11 +313,6 @@ class clsCSV {
                     return
                 }
             }
-
-            if (this.layout._IDIsInsideHeader(divID)) {
-                if (divID == "header-Tags") {
-                    this.layout.DowpDown_ShowHide("Tags")}
-                return}
 
             if (this.layout._IDIsInput(divID)) {
                 return}
@@ -404,18 +399,6 @@ class clsCSV {
         div.append(input);
         this.InputFiled_AutoHeight();
         this.layout.div_input = input;
-    }
-
-    _CreateSaveSVG(divID) {
-        let div = document.getElementById(divID);
-        let a = document.createElement('a');
-        a.href = "#"
-        a.setAttribute('onclick', this.name + '.SaveEdit()');
-        a.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save m-2" viewBox="0 0 16 16"> \
-        <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 \
-        3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/> \
-        </svg>'
-        div.append(a);
     }
 
     _CreateRevertX(divID) {
@@ -548,6 +531,7 @@ class clsCSV {
         for (let header of this.headers) {
             if (header == "Tags") {
                 ret += '<th id = "header-' + header + '" class="ecsvtable col-' + header + '" '+ colwidth[header] +'>' + header
+                ret += " " + this._svgText_ArrowDown()
                 ret += this.AddTagMenu()
                 ret += '</th>'}
             else {
@@ -813,6 +797,28 @@ class clsCSV {
             
         }
         return ret
+    }
+
+    _svgAppend_Save(divID) {
+        let div = document.getElementById(divID);
+        let a = document.createElement('a');
+        a.href = "#"
+        a.setAttribute('onclick', this.name + '.SaveEdit()');
+        a.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save m-2" viewBox="0 0 16 16"> \
+        <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 \
+        3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/> \
+        </svg>'
+        div.append(a);
+    }
+
+    _svgText_ArrowDown(){
+        let para = "ecsv.layout.DowpDown_ShowHide('Tags')"
+        let param = '"' + para + '"'
+        return '<a href="#" onclick=' + param + '>\
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">\
+        <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0\
+         0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>\
+      </svg></a>'
     }
 
 // ################################################################
